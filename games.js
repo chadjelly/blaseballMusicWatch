5cb947e83af886395825602f51dc264da2a1047a
chadjelly <88442520+chadjelly@users.noreply.github.com>
2021-08-04 08:14:03 -0700

    Update games.js

--- games.js
+++ games.js
@@ -20,7 +20,7 @@ function draw() {
   background(0);
   fill(255);
   
-  if(games != null) {  
+  if(games[0] != null) {  
     text(games[0].lastUpdate + String.fromCodePoint(games[0].homeTeamEmoji), 10, 30);
   }
 }
